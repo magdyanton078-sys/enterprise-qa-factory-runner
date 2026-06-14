2478c06fdcbf1d10b379069b1dff9547d1275524
magdyanton078-sys <magdyanton078@gmail.com>
2026-06-14 12:23:38 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,14 @@
+import { defineConfig } from '@playwright/test';
 
+export default defineConfig({
+
+  testDir: './runner',
+
+  use: {
+    headless: true
+  },
+
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }]
+  ]
+});
